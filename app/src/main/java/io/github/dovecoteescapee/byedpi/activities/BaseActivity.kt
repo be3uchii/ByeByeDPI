17ce2a0c8982bc62dbf8694e848e54a6b0b5e57d
be3uchii <standofd2axselboltnice19@gmail.com>
2026-01-09 18:26:01 +0300

    Update BaseActivity.kt
    Signed-off-by: be3uchii <standofd2axselboltnice19@gmail.com>

--- app/src/main/java/io/github/dovecoteescapee/byedpi/activities/BaseActivity.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/activities/BaseActivity.kt
@@ -1,34 +1,12 @@
 package io.github.dovecoteescapee.byedpi.activities
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.LocaleListCompat
-import io.github.dovecoteescapee.byedpi.utility.getPreferences
-import io.github.dovecoteescapee.byedpi.utility.getStringNotNull
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val prefs = getPreferences()
-        
-        // Установка языка
-        val lang = prefs.getStringNotNull("language", "system")
-        val appLocale = when (lang) {
-            "ru" -> LocaleListCompat.forLanguageTags("ru")
-            "en" -> LocaleListCompat.forLanguageTags("en")
-            else -> LocaleListCompat.getEmptyLocaleList()
-        }
-        AppCompatDelegate.setApplicationLocales(appLocale)
-
-        // Установка темы
-        val theme = prefs.getStringNotNull("app_theme", "system")
-        val mode = when (theme) {
-            "light" -> AppCompatDelegate.MODE_NIGHT_NO
-            "dark" -> AppCompatDelegate.MODE_NIGHT_YES
-            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-        }
-        AppCompatDelegate.setDefaultNightMode(mode)
-
         super.onCreate(savedInstanceState)
+        // Тут больше ничего не нужно. 
+        // Приложение будет использовать системный язык и тему.
     }
 }
