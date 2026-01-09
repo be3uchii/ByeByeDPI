3d274766dcf9ed97b6cd19a421c0300872d65b6e
be3uchii <standofd2axselboltnice19@gmail.com>
2026-01-09 16:34:59 +0300

    Update BaseActivity.kt
    Signed-off-by: be3uchii <standofd2axselboltnice19@gmail.com>

--- app/src/main/java/io/github/dovecoteescapee/byedpi/activities/BaseActivity.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/activities/BaseActivity.kt
@@ -3,8 +3,9 @@ package io.github.dovecoteescapee.byedpi.activities
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.google.android.material.color.DynamicColors
-import io.github.dovecoteescapee.byedpi.fragments.MainSettingsFragment
 import io.github.dovecoteescapee.byedpi.utility.getPreferences
 import io.github.dovecoteescapee.byedpi.utility.getStringNotNull
 
@@ -14,10 +15,10 @@ abstract class BaseActivity : AppCompatActivity() {
         val prefs = getPreferences()
 
         val lang = prefs.getStringNotNull("language", "system")
-        MainSettingsFragment.setLang(lang)
+        setLang(lang)
 
         val theme = prefs.getStringNotNull("app_theme", "system")
-        MainSettingsFragment.setTheme(theme)
+        setTheme(theme)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             DynamicColors.applyToActivityIfAvailable(this)
@@ -26,4 +27,33 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
     }
 
+    private fun setLang(lang: String) {
+        val appLocale = localeByName(lang) ?: return
+        if (AppCompatDelegate.getApplicationLocales().toLanguageTags() != appLocale.toLanguageTags()) {
+            AppCompatDelegate.setApplicationLocales(appLocale)
+        }
+    }
+
+    private fun setTheme(name: String) {
+        val appTheme = themeByName(name) ?: return
+        if (AppCompatDelegate.getDefaultNightMode() != appTheme) {
+            AppCompatDelegate.setDefaultNightMode(appTheme)
+        }
+    }
+
+    private fun localeByName(lang: String): LocaleListCompat? = when (lang) {
+        "system" -> LocaleListCompat.getEmptyLocaleList()
+        "ru" -> LocaleListCompat.forLanguageTags("ru")
+        "en" -> LocaleListCompat.forLanguageTags("en")
+        "tr" -> LocaleListCompat.forLanguageTags("tr")
+        "kk" -> LocaleListCompat.forLanguageTags("kk")
+        else -> null
+    }
+
+    private fun themeByName(name: String): Int? = when (name) {
+        "system" -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        "light" -> AppCompatDelegate.MODE_NIGHT_NO
+        "dark" -> AppCompatDelegate.MODE_NIGHT_YES
+        else -> null
+    }
 }
